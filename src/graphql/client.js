9c8e7e70ef35cff46790855bb319de1b776d96fe
Đỗ Đức Anh <anhdott1@gmail.com>
2025-09-06 22:26:54 +0700

    change url

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,5 +1,11 @@
 import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client";
+
+
+
+// For Vite, use import.meta.env.VITE_REACT_APP_GRAPHQL_URL only
+const GRAPHQL_URL = import.meta.env.VITE_REACT_APP_GRAPHQL_URL;
+
 export const client = new ApolloClient({
-  link: new HttpLink({ uri: "http://localhost:3000/graphql" }),
+  link: new HttpLink({ uri: GRAPHQL_URL }),
   cache: new InMemoryCache(),
 });
